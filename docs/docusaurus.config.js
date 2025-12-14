5fd6ead6769c6eef88ea9d0b4971fbed8694d6c8
Rex Liu <hi@r3x.sh>
2025-12-14 04:48:10 -0500

    docs: adopt grayscale theme with accent

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -74,7 +74,7 @@ const config = {
                         ],
                     },
                     {
-                        title: "GitHub",
+                        title: "Links",
                         items: [
                             {
                                 label: "Issues",
